a389380457938229f3b69141986485b047dcd819
bin <tinnt0504@gmail.com>
2024-05-27 15:08:01 +0700

    fix filter for bookResolver

--- src/dtos/book/filter-book.dto.ts
+++ src/dtos/book/filter-book.dto.ts
@@ -1,46 +1,33 @@
 import { InputType, Field, Int, ArgsType } from '@nestjs/graphql';
+import { IsArray, IsBoolean, IsNumber, IsString } from 'class-validator';
 
-@InputType()
+@ArgsType()
 export class FilterBookDto {
   @Field(() => String,{ nullable: true })
+  @IsString()
   title?: string;
 
   @Field(() => [Int], { nullable: true })
+  @IsArray()
   rate?: number[];
 
   @Field(() => [String],{ nullable: true })
+  @IsArray()
   author?: string[];
 
   @Field(() => [String], { nullable: true })
+  @IsArray()
   category?: string[];
 
-  @Field(() => Boolean, { nullable: true })
+  @Field(() => Boolean, { nullable: true , defaultValue: true})
+  @IsBoolean()
   isActive?: boolean
 
   @Field(() => Int, { nullable: true })
+  @IsNumber()
   page?: number;
 
   @Field(() => Int,{ nullable: true })
+  @IsNumber()
   limit?: number;
 }
-
-// @ArgsType()
-// export class getBookArgs {
-//   @Field(() => String,{ nullable: true })
-//   title?: string;
-
-//   @Field(() => Int, { nullable: true })
-//   rate?: number;
-
-//   @Field(() => [String],{ nullable: true })
-//   author?: Array<string>;
-
-//   @Field(() => [String], { nullable: true })
-//   category?: Array<string>;
-
-//   @Field(() => Int, { nullable: true })
-//   page?: number;
-
-//   @Field(() => Int,{ nullable: true })
-//   limit?: number;
-// }
--- src/modules/customer/book/book.resolver.ts
+++ src/modules/customer/book/book.resolver.ts
@@ -10,26 +10,9 @@ import { BookService } from 'src/shared/services/book/book.service';
 export class BookResolver {
   constructor(private readonly bookService: BookService) {}
 
-  //BAD PRACTICE
-  @Query(() => ResponseBookDto)
-  async findAllBooks(
-    @Args('title', { nullable: true }) title?: string,
-    @Args('rate', { nullable: true, type: () => [Int] }) rate?: number[],
-    @Args('author', { nullable: true, type: () => [String] }) author?: string[],
-    @Args('category', { nullable: true, type: () => [String] }) category?: string[],
-    @Args('page', { nullable: true, type: () => Int, defaultValue: 1} ) page?: number,
-    @Args('limit', { nullable: true, type: () => Int, defaultValue: 12 }) limit?: number
-  ) {
+@Query(() => ResponseBookDto)
+  async findAllBooks(@Args() filter: FilterBookDto) {
     try {
-      let filter = plainToInstance(FilterBookDto, {
-        title,
-        rate,
-        author,
-        category,
-        isActive: true,
-        page,
-        limit
-      });
       const result = await this.bookService.findAll(filter);
       const response = plainToInstance(ResponseBookDto, result);
       return response;
@@ -39,17 +22,3 @@ export class BookResolver {
     }
   }
 }
-
-// @Query(() => ResponseBookDto)
-//   async findAllBooks(@Args() filter: getBookArgs) {
-//     console.log('Searching', filter);
-//     try {
-//       const result = await this.bookService.findAllForCustomer(filter);
-//       const response = plainToInstance(ResponseBookDto, result);
-//       return response;
-//     } catch (error) {
-//       console.error(error);
-//       throw new Error('Failed to fetch books');
-//     }
-//   }
-// }
