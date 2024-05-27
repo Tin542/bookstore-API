4f0414a11e2550dac842b67a726218781bc8bb16
bin <tinnt0504@gmail.com>
2024-05-27 15:11:46 +0700

    add optional to filter

--- src/dtos/book/filter-book.dto.ts
+++ src/dtos/book/filter-book.dto.ts
@@ -1,33 +1,40 @@
 import { InputType, Field, Int, ArgsType } from '@nestjs/graphql';
-import { IsArray, IsBoolean, IsNumber, IsString } from 'class-validator';
+import { IsArray, IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
 
 @ArgsType()
 export class FilterBookDto {
   @Field(() => String,{ nullable: true })
   @IsString()
+  @IsOptional()
   title?: string;
 
   @Field(() => [Int], { nullable: true })
   @IsArray()
+  @IsOptional()
   rate?: number[];
 
   @Field(() => [String],{ nullable: true })
   @IsArray()
+  @IsOptional()
   author?: string[];
 
   @Field(() => [String], { nullable: true })
   @IsArray()
+  @IsOptional()
   category?: string[];
 
   @Field(() => Boolean, { nullable: true , defaultValue: true})
   @IsBoolean()
+  @IsOptional()
   isActive?: boolean
 
   @Field(() => Int, { nullable: true })
   @IsNumber()
+  @IsOptional()
   page?: number;
 
   @Field(() => Int,{ nullable: true })
   @IsNumber()
+  @IsOptional()
   limit?: number;
 }
