5d52a70013a04487caa7d93b9d0e8057c7f75f0c
bin <tinnt0504@gmail.com>
2024-05-31 23:55:10 +0700

    order

---
+++ src/entities/order.entity.ts
@@ -0,0 +1,33 @@
+
+import { Field, Float, ObjectType } from '@nestjs/graphql';
+import { Order, OrderStatus, PaymentMethod } from '@prisma/client';
+
+@ObjectType()
+export class OrderEntity {
+  @Field(() => String)
+  id: Order['id'];
+
+  @Field(() => Float)
+  totalPrice: Order['totalPrice'];
+
+  @Field(() => String)
+  userId: Order['userId'];
+
+  @Field(() => OrderStatus)
+  status: Order['status'];
+
+  @Field(() => Date)
+  paidAt: Order['paidAt'];
+
+  @Field(() => String)
+  address: Order['address'];
+
+  @Field(() => String)
+  customerName: Order['customerName'];
+
+  @Field(() => String)
+  phoneNumber: Order['phoneNumber'];
+
+  @Field(() => PaymentMethod)
+  paymentMethod: Order['paymentMethod'];
+}
